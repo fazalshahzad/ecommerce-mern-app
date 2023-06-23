de306e08a383d177e6008ce376d08d9b77d871ad
fazalshahzad <shancompany322@gmail.com>
2023-06-23 19:41:29 +0500

    c

--- client/src/components/Layout/Routes/private.js
+++ client/src/components/Layout/Routes/private.js
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import {useAuth} from "../../../context/auth"
+import { Outlet } from "react-router-dom";
 
 
 export default function PrivateRoute () {
 
     const [ok, setOk] = useState(false)
     const [auth, setAuth] = useAuth()
+
+
+    useEffect(()=> {},[])
+    return ok ? <Outlet/> :'spinner'
+
 }
