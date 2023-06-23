2183e09bc0e54a7571b6c3e63e55e57daae75d24
fazalshahzad <shancompany322@gmail.com>
2023-06-23 20:18:30 +0500

    c

--- client/src/components/Layout/Routes/private.js
+++ client/src/components/Layout/Routes/private.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import {useAuth} from "../../../context/auth"
 import { Outlet } from "react-router-dom";
+import axios from "axios";
 
 
 export default function PrivateRoute () {
@@ -9,7 +10,23 @@ export default function PrivateRoute () {
     const [auth, setAuth] = useAuth()
 
 
-    useEffect(()=> {},[])
+    useEffect(()=> {
+        const authCheck = async() => {
+            const res = await axios.get('/api/v1//auth/user-auth');
+
+
+            if(res.data.ok){
+                setOk(true)
+            }
+            else{
+                setOk(false)
+            }
+
+        }
+
+        if(auth?.token) authCheck()
+
+    },[auth?.token])
     return ok ? <Outlet/> :'spinner'
 
 }
